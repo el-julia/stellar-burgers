7de84bc090013fb59eded548c8a792de81e40f52
Julia Shkolina <jsk108@yandex.ru>
2025-06-18 20:30:01 +0400

    настроила роуты

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,39 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import { AppHeader, Modal, OrderInfo } from '@components';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 const App = () => (
+
+  const location = useLocation();
+  const background = location.state?.background;
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes location={background || location}>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='/ingredients/:id' element={<IngredientsDetails />} />
+      <Route path='/feed/:number' element={<OrderInfo />} />
+      <Route path='/profile/orders/:number' element={<OrderInfo />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
