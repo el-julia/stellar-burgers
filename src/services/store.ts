c5bd5d3ceb55e0ff775e0965dbc079e78ebd4e2d
Julia Shkolina <jsk108@yandex.ru>
2025-06-19 13:08:28 +0400

    добавила в rootReducer ingredientsReducer

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import {
   TypedUseSelectorHook,
@@ -6,7 +6,9 @@ import {
   useSelector as selectorHook
 } from 'react-redux';
 
-const rootReducer = () => {}; // Заменить на импорт настоящего редьюсера
+export const rootReducer = combineReducers({
+  ingredients: ingredientsReducer
+}); // Заменить на импорт настоящего редьюсера
 
 const store = configureStore({
   reducer: rootReducer,
