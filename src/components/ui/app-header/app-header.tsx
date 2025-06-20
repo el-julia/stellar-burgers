61ef174d78e9f5e79cb6b6ce392de6cdd14cb262
Julia Shkolina <jsk108@yandex.ru>
2025-06-20 20:02:57 +0400

    убрала лишние элементы

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -13,40 +13,34 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
-          <NavLink
-            to='/'
-            className={({ isActive }) =>
-              isActive ? styles.link + ' ' + styles.link_active : styles.link
-            }
-          >
-            {({ isActive }) => (
-              <>
-                <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
-                <p className='text text_type_main-default ml-2 mr-10'>
-                  Конструктор
-                </p>
-              </>
-            )}
-          </NavLink>
-        </>
-        <>
-          <NavLink
-            to='/feed'
-            className={({ isActive }) =>
-              isActive ? styles.link + ' ' + styles.link_active : styles.link
-            }
-          >
-            {({ isActive }) => (
-              <>
-                <ListIcon type={isActive ? 'primary' : 'secondary'} />
-                <p className='text text_type_main-default ml-2'>
-                  Лента заказов
-                </p>
-              </>
-            )}
-          </NavLink>
-        </>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            isActive ? styles.link + ' ' + styles.link_active : styles.link
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2 mr-10'>
+                Конструктор
+              </p>
+            </>
+          )}
+        </NavLink>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            isActive ? styles.link + ' ' + styles.link_active : styles.link
+          }
+        >
+          {({ isActive }) => (
+            <>
+              <ListIcon type={isActive ? 'primary' : 'secondary'} />
+              <p className='text text_type_main-default ml-2'>Лента заказов</p>
+            </>
+          )}
+        </NavLink>
       </div>
       <Link to='/' className={styles.logo}>
         <Logo className='' />
