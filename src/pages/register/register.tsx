a3c9d60668f9cb9f417722ae9d3c0828a7e18735
Julia Shkolina <jsk108@yandex.ru>
2025-06-22 16:29:52 +0400

    реализовала регистрацию

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,46 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useAppSelector, useDispatch } from '../../services/store';
+import {
+  register,
+  selectErrorMessage,
+  selectIsLoading,
+  selectUser
+} from '../../services/slices/profile-slices';
+import { useNavigate } from 'react-router-dom';
+import { Preloader } from '@ui';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const errorMessage = useAppSelector(selectErrorMessage);
+  const user = useAppSelector(selectUser);
+  const navigate = useNavigate();
+  const isLoading = useAppSelector(selectIsLoading);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/profile', { replace: true });
+    }
+  }, [user, navigate]);
 
   const handleSubmit = (e: SyntheticEvent) => {
+    let data = {
+      name: userName,
+      email,
+      password
+    };
+    dispatch(register(data));
     e.preventDefault();
   };
 
-  return (
+  return isLoading ? (
+    <Preloader />
+  ) : (
     <RegisterUI
-      errorText=''
+      errorText={errorMessage ?? undefined}
       email={email}
       userName={userName}
       password={password}
